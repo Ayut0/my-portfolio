74afd14082aff979b056618e78aa4070f49fb04a
Yuto Yamakita <yuvto.0420@gmail.com>
2022-11-01 20:50:22 -0700

    finished hero page

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,16 +1,22 @@
 import { motion } from 'framer-motion'
-import React from 'react'
+import { NextPage } from 'next'
+import React, { ReactNode } from 'react'
 import { liVariants } from './MobileMenu'
 
-const Button = () => {
+type Props = {
+  children: ReactNode
+} & React.ComponentProps<'a'>
+
+const Button: React.FC<Props> = ({children}) => {
   return (
     <motion.button
-      whileHover={{ scale: 1.1, color: '#fff' }}
-          whileTap={{ scale: 0.9 }}
-          variants={liVariants}
-      className=' bg-transparent outline-double outline-headline-dark font-semibold py-1.5 px-4 rounded h-fit'
-      >
-          <a href='../../assets/RESUME(YUTO YAMAKITA).pdf' download>RESUME</a>
+      whileHover={{ scale: 1.1, color: '#0F0E17', backgroundColor: '#FF8906' }}
+      whileTap={{ scale: 0.9 }}
+      variants={liVariants}
+      className=' bg-transparent outline-double border-2 border-black outline-headline-dark font-semibold py-1.5 px-4 rounded h-fit text-headline-dark'
+    >
+
+      {children}
     </motion.button>
   )
 }
