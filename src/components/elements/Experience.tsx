cbfc8ae4d75d4af336fbdbee864b2aa5742bfd19
Yuto Yama <yuvto.0420@gmail.com>
2023-11-06 15:30:04 -0800

    Changhe the position title

--- src/components/elements/Experience.tsx
+++ src/components/elements/Experience.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WorkExperience = () => {
   return (
     <div className='flex flex-col gap-y-[16px]'>
-      <h3 className='font-headline font-bold text-2xl md:text-5xl'>Frontend Developer Intern</h3>
+      <h3 className='font-headline font-bold text-2xl md:text-5xl'>Frontend Developer</h3>
       <span className='font-semibold md:text-3xl'>80&Company</span>
       <div className='font-content flex flex-col pb-4 md:text-2xl'>
         <span>2023.01 - Currently Working</span>
