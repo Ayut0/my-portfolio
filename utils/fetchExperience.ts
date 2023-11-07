9b99173af6a7679df102b8fa022fdf804bf3069e
Yuto Yama <yuvto.0420@gmail.com>
2023-11-07 08:17:36 -0800

    Edit api endpoint

--- utils/fetchExperience.ts
+++ utils/fetchExperience.ts
@@ -1,27 +1,27 @@
 import { groq } from 'next-sanity'
 import { sanityClient } from 'sanity'
+import { apiEndpoint } from '@/const/url'
 import type { WorkExperience } from 'typings'
 
-const query = groq`
-  ${process.env.NEXT_PUBLIC_FETCH_EXPERIENCE}
-`
+
+// const query = groq`
+//   ${process.env.NEXT_PUBLIC_FETCH_EXPERIENCE}
+// `
 
 type Data = {
   workExperience: WorkExperience[]
 }
 
-export const fetchExperience = async () => {
-  const experience: WorkExperience[] = await sanityClient.fetch(query)
-  return experience
-}
-
-// import { WorkExperience } from "typings";
-
 // export const fetchExperience = async () => {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`)
-//   const data = await res.json()
-//   console.log(data);
-//   const experience: WorkExperience[] = data.workExperience
-  
-//   return experience;
+//   const experience: WorkExperience[] = await sanityClient.fetch(query)
+//   return experience
 // }
+
+
+export const fetchExperience = async () => {
+  const res = await fetch(`${apiEndpoint}/api/getExperience`)
+  const data = await res.json()
+  const experience: WorkExperience[] = data.workExperience
+  
+  return experience;
+}
