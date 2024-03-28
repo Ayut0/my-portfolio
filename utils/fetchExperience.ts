8927e8fbb7712428177067927a022464f0222b15
Yuto Yama <yuvto.0420@gmail.com>
2024-03-27 22:01:33 -0700

    fix fetch experience

--- utils/fetchExperience.ts
+++ utils/fetchExperience.ts
@@ -1,27 +1,27 @@
 import { groq } from 'next-sanity'
-import { sanityClient } from 'sanity'
 import { apiEndpoint } from '@/const/url'
+import { sanityClient } from 'sanity'
 import type { WorkExperience } from 'typings'
 
 
-// const query = groq`
-//   ${process.env.NEXT_PUBLIC_FETCH_EXPERIENCE}
-// `
+const query = groq`
+  ${process.env.NEXT_PUBLIC_FETCH_EXPERIENCE}
+`
 
 type Data = {
   workExperience: WorkExperience[]
 }
 
-// export const fetchExperience = async () => {
-//   const experience: WorkExperience[] = await sanityClient.fetch(query)
-//   return experience
-// }
-
-
 export const fetchExperience = async () => {
-  const res = await fetch(`${apiEndpoint}/api/getExperience`)
-  const data = await res.json()
-  const experience: WorkExperience[] = data.workExperience
-  
-  return experience;
+  const experience: WorkExperience[] = await sanityClient.fetch(query)
+  return experience
 }
+
+
+// export const fetchExperience = async () => {
+//   const res = await fetch(`${apiEndpoint}/api/getExperience`)
+//   const data = await res.json()
+//   const experience: WorkExperience[] = data.workExperience
+  
+//   return experience;
+// }
